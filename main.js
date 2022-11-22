2470ebc5da561ae59e76df4df9e4175080bd32cb
Samaksh-Rawat <97016925+Samaksh-Rawat@users.noreply.github.com>
2022-11-22 17:48:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function setup()
 }
 img="";
 status="";
+objects=[];
 function preload()
 {
     img=loadImage("dog_cat.jpg")
@@ -14,18 +15,22 @@ function preload()
 function draw()
 {
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("Dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
 
-    fill("#05f545");
-    text("Cat",320,120)
-    noFill();
-    stroke("#05f545");
-    rect(300,95,290,290);
-}
+    if(status !="")
+    {
+        for(i = 0; i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label+" "+ percent + "%", objects[i].x,objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
+ }
 function modelLoaded()
 {
     console.log("model Loaded");
@@ -38,4 +43,5 @@ function gotResults(error,results)
     console.log(error);
     }
     console.log(results);
+    objects=results;
 }
